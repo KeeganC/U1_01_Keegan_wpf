921d64586990e1936fa0be3f6dfd12e59f8031b8
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-02-07 09:44:06 -0500

    Create MainWindow.xaml.cs
    Added cs file

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+/*
+ * Keegan Chan
+ * Feb 6th 2018
+ * U1_01_Keegan_wpf
+ * Hello world - my first program
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U1_01_Keegan_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            lblOutput.Content = "Yo, how you doin'?";
+        }
+    }
+}
